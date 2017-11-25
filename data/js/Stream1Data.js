e86f20c272099fde88ecaa369436a09eac98c15a
gosiaol <gosiek_ol@wp.pl>
2017-11-25 12:21:06 +0100

    Form changes

--- data/js/Stream1Data.js
+++ data/js/Stream1Data.js
@@ -1,6 +1,12 @@
 var Stream1Data= function () {
-    this.type=$('#source').val();
+    this.type=$('#sink').val();
     this.path=$('#file').val();
-
+    this.fps=$('#fps').val();
+    this.width=$('#width').val();
+    this.height=$('#height').val();
+    this.bitRateAudio=$('#audio_bitrate').val();
+    this.audioCodec=$('#acodec').val();
+    this.bitRateVideo=$('#video_bitrate').val();
+    this.videoCodec=$('#vcodec').val();
 
 }
--- data/js/Stream2Data.js
+++
@@ -1,7 +0,0 @@
-var Stream2Data = function () {
-	   this.audioCodec=$('#audioCodec').val();
-       this.bitRateAudio=$('#bitRateAudio').val();
-       this.videoCodec=$('#videoCodec').val();
-       this.bitRateVideo=$('#bitRateVideo').val();
-       this.fps=$('#fps').val();
-}
