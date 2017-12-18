2bd8df8f622719db4bce194bc5a869bacc4740ce
gosiaol <gosiek_ol@wp.pl>
2017-12-18 23:59:15 +0100

    adding stream

--- data/js/StreamData.js
+++ data/js/StreamData.js
@@ -10,8 +10,10 @@ var StreamData= function () {
     if($('#width').val()!==""){ this.width=$('#width').val();}
     if ($('#height').val()!==""){this.height=$('#height').val();}
     if ($('#audio_bitrate').val()!==""){this.audio_bitrate=$('#audio_bitrate').val();}
+    if ($('#audio_stream').val()!==""){this.audio_stream=$('#audio_stream').val();}
     if ($('#acodec').val()!==null){this.acodec=$('#acodec').val();}
     if ($('#video_bitrate').val()!==""){this.video_bitrate=$('#video_bitrate').val();}
+    if ($('#video_stream').val()!==""){this.video_stream=$('#video_stream').val();}
     if ($('#vcodec').val()!==null){this.vcodec=$('#vcodec').val();}
     this.random=(function(m,s,c){return (c ? arguments.callee(m,s,c-1) : '#') + s[m.floor(m.random() * s.length)]})(Math,'0123456789ABCDEF',12);
 
