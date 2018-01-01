9eceffb5bbcded8b035fce667c4ac5b296eb11fd
jgorski <jw.gor07@gmail.com>
2018-01-01 22:50:18 +0100

    Changed to collapse

--- data/js/InfoStream.js
+++ data/js/InfoStream.js
@@ -3,45 +3,42 @@ InfoStream = function (InfoData) {
 
     console.log(InfoData);
 
-    $.post('http://' + window.location.hostname + ':8090/info',
+    $.post('http://' + window.location.hostname + ':8090' + '/info',
         JSON.stringify(InfoData),
         function (data, status) {
-            //var resultHtml = $("<div class=\"container-fluid\">");
-            var resultHtml = $("<div class=\"row\">")
-            //var result = JSON.stringify(data)
+            $( "#accordion" ).html( "" );
             for (i = 0; i < data["audio"].length; i++) {
-
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " AUDIO </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["audio"][i]["audio"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " STREAMID </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["audio"][i]["streamid"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " BITRATE </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["audio"][i]["bitrate"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " DEPTH </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["audio"][i]["depth"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " SAMPLE_RATE </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["audio"][i]["sample_rate"] + "</p></div>")
+                var resultHtmls = $("<div class=\"panel panel-default\"><div class=\"panel-heading\"><h5 class=\"panel-title\"><a data-toggle=\"collapse\" href=\"#audio"+i+"\">Audio "+ i +"</a></h5></div>");
+                var resultHtml=  $("<div id=\"audio"+i+"\"class=\"panel-collapse collapse\">");
+                //var row = $("<div class=\"row\">");
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "AUDIO</div> <div class=\"col-sm-9\">"+"<p>"+data["audio"][i]["audio"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "STREAMID</div> <div class=\"col-sm-9\">"+"<p>"+data["audio"][i]["streamid"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "BITRATE</div> <div class=\"col-sm-9\">"+"<p>"+data["audio"][i]["bitrate"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "DEPTH</div> <div class=\"col-sm-9\">"+"<p>"+data["audio"][i]["depth"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "SAMPLE_RATE</div> <div class=\"col-sm-9\">"+"<p>"+data["audio"][i]["sample_rate"] +"</p></div></div>")
+                resultHtml.append("</div>")
+                resultHtmls.append(resultHtml)
+                resultHtmls.append("</div>")
+                $( "#accordion" ).append( resultHtmls );
             }
-            resultHtml.append("</div>");
-            resultHtml.append("<div class=\"row\">")
             for (i = 0; i < data["video"].length; i++) {
-
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " VIDEO </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["video"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " STREAMID </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["streamid"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " BITRATE </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["bitrate"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " DEPTH </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["depth"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " HEIGHT </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["height"] + "</p></div>")
-                resultHtml.append("<div class=\"col-sm-2\"" +"\">" +  " WIDTH </div>")
-                resultHtml.append("<div class=\"col-sm-9\"" +"\">" + "<p>" +data["video"][i]["witdh"] + "</p></div>")
+                var resultHtmls = $("<div class=\"panel panel-default\"><div class=\"panel-heading\"><h5 class=\"panel-title\"><a data-toggle=\"collapse\" href=\"#video"+i+"\">Video "+ i +"</a></h5></div>");
+                var resultHtml=  $("<div id=\"video"+i+"\"class=\"panel-collapse collapse\">");
+                //var row = $("<div class=\"row\">");
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "VIDEO</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["video"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "STREAMID</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["streamid"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "BITRATE</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["bitrate"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "DEPTH</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["depth"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "HEIGHT</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["height"] +"</p></div></div>")
+                resultHtml.append("<div class=\"row\"><div class=\"col-sm-2\">" +  "WIDTH</div> <div class=\"col-sm-9\">"+"<p>"+data["video"][i]["witdh"] +"</p></div></div>")
+                resultHtml.append("</div>")
+                resultHtmls.append(resultHtml)
+                resultHtmls.append("</div>")
+                $( "#accordion" ).append( resultHtmls );
             }
-            resultHtml.append("</div>");
-            //resultHtml.append("</div>");
-            $("#message").html(resultHtml);
-            
+
+            console.log(resultHtml);
+
+
         },"json");
 }
